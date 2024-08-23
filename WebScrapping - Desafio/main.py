09b99116ace425c843548649db6bcbf7b83aa9a8
Ackin.Dev <ackinlino123@gmail.com>
2024-08-23 17:27:08 -0300

    Bug Fixed

--- WebScrapping - Desafio/main.py
+++ WebScrapping - Desafio/main.py
@@ -10,16 +10,34 @@ hora_extracao = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 bsp_texto = BeautifulSoup(texto_string, 'html.parser')
 lista_noticias = bsp_texto.find_all('div', attrs={'class':'feed-post-body'})
 
+lista_noticias = bsp_texto.find_all('div', attrs={'class': 'feed-post-body'})
+
+
 print("Quantidade de manchetes = ", len(lista_noticias))
 
 print(lista_noticias[0].contents[1].text.replace('"',""))
 
 print(lista_noticias[0].find('a').get('href'))
+print()
 
 descricao = lista_noticias[0].contents[2].text
 
-"""if not descricao:
-    descricao = noticia.find('div',attrs={'class': 'bstn-related'})
-    descricao = descricao.text if descricao else None
-print(descricao)"""    
+# Acessar e exibir a primeira notícia
+if lista_noticias:
+    primeira_noticia = lista_noticias[0]
+    
+    # Exibir o texto da manchete
+    manchete = primeira_noticia.find('a').text.strip()
+    print(manchete)
+    
+    # Exibir o link da notícia
+    link = primeira_noticia.find('a').get('href')
+    print(link)
+    
+    # Exibir a descrição da notícia
+    descricao = primeira_noticia.find('div', attrs={'class': 'feed-post-body-resumo'})
+    descricao = descricao.text.strip() if descricao else "Descrição não disponível"
+    print(descricao)
+else:
+    print("Nenhuma notícia encontrada.")  
     
